f92bed10aaaf413024ae0078a30599d6a2c64521
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-04-12 15:47:33 -0600

    general back up after typescript implement login

--- server/controllers/users-controller.ts
+++ server/controllers/users-controller.ts
@@ -2,11 +2,33 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
-module.exports = {
-    //User resolver
-    async user(parent, { username }, context) => {
-    return User.findOne({username}).populate('conversation');
-    },
+//User resolver
+// export const user = async(parent:any, { username }, context:any) => {
+//     return User.findOne({username});
+// }
 
-    
-}
+
+//interface login params 
+interface loginParams {
+    email: string, 
+    password: string,
+};
+
+//Login Mutation 
+export const login = async(_:any, {email, password}:loginParams) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+};
