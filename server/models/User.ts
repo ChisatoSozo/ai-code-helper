b4f4fb3197687184e5dc09b38a41e369baf2f725
moon <rycmoon@gmail.com>
2022-04-12 13:01:12 -0500

    added user

---
+++ server/models/User.ts
@@ -0,0 +1,39 @@
+import {Schema,model} from 'mongoose'
+import {hash,compare} from 'bcrypt'
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  }
+)
+
+// hash user password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await hash(this.password, saltRounds);
+  }
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async (password:string)=> {
+    //@ts-ignore
+    return compare(password, this.password);
+};
+
+export const User = model('User',userSchema)
