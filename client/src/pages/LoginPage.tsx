ee3bcade832aad8c8d5f0567521f52b8fbf39381
moon <rycmoon@gmail.com>
2022-04-12 19:05:39 -0500

    set up pages

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,70 +1,11 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { LOGIN } from '../utils';
-import { useNavigate } from 'react-router-dom';
-
-interface ILoginForm {
-  email: string;
-  password: string;
-}
+import React from 'react';
+import { LoginForm } from '../components/LoginForm';
 
 export const LoginPage = () => {
-
-  const navigation = useNavigate()
-  const [login,{error:loginError}] = useMutation(LOGIN)
-
-  const [loginForm, setLoginForm] = useState<ILoginForm>({
-    email: '',
-    password: '',
-  });
-  const { email, password } = loginForm;
-
-  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLoginForm((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
-    if(!email || !password){
-      return
-    }
-
-    try{
-      //TODO
-      const {data} = await login({variables:{...loginForm}})
-      navigation('/messenger')
-    }catch (e) {
-      console.log(e)
-    }
-
-  }
-
   return (
-    <>
-      <form>
-      <label htmlFor={'email'}>Email:</label>
-      <input
-        onChange={handleTextChange}
-        name={'email'}
-        placeholder={'email'}
-        type={'email'}
-        value={email}
-      />
-      <label htmlFor={'password'}>Password:</label>
-      <input
-        onChange={handleTextChange}
-        name={'password'}
-        placeholder={'password'}
-        type={'password'}
-        value={password}
-      />
-        <button>Create Account</button>
-      </form>
-      {loginError && (<p>Error Logging in</p>)}
-    </>
+    <div>
+      <LoginForm/>
+    </div>
   );
 };
+
--- client/src/pages/SignupPage.tsx
+++ client/src/pages/SignupPage.tsx
@@ -1,105 +1,11 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { CREATE_ACCOUNT } from '../utils';
-import { useNavigate } from 'react-router-dom';
-
-interface ISignupForm {
-  username: string,
-  email: string,
-  password: string
-  verifyPassword: string
-}
-
+import React from 'react';
+import { SignupForm } from '../components/SignupForm';
 
 export const SignupPage = () => {
-
-
-  const navigation = useNavigate()
-  const [createAccount, { error: createAccountError }] = useMutation(CREATE_ACCOUNT);
-  const [error, setError] = useState<string>('');
-
-  const [signupForm, setSignupForm] = useState<ISignupForm>({
-    email: '',
-    password: '',
-    verifyPassword: '',
-    username: ''
-  });
-  const { email, password, verifyPassword, username } = signupForm;
-
-  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSignupForm((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value
-    }));
-
-    if (!e.target.value.length) {
-      setError(`${e.target.name} is required`);
-    } else {
-      setError('');
-    }
-
-    if (e.target.name === 'verifyPassword' && password !== verifyPassword) {
-      setError('passwords do not match');
-    } else {
-      setError('');
-    }
-
-  };
-
-  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (error) {
-      return;
-    }
-    try {
-      //TODO
-      const { data } = await createAccount({ variables: { ...signupForm } });
-      navigation('/messenger')
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-
   return (
-    <>
-      <form onSubmit={handleFormSubmit}>
-        <label htmlFor={'username'}>Username:</label>
-        <input
-          onChange={handleTextChange}
-          name={'username'}
-          placeholder={'username'}
-          type={'username'}
-          value={username}
-        />
-        <label htmlFor={'email'}>Email:</label>
-        <input
-          onChange={handleTextChange}
-          name={'email'}
-          placeholder={'email'}
-          type={'email'}
-          value={email}
-        />
-        <label htmlFor={'password'}>Password:</label>
-        <input
-          onChange={handleTextChange}
-          name={'password'}
-          placeholder={'password'}
-          type={'password'}
-          value={password}
-        />
-        <label htmlFor={'verifyPassword'}>Verify Password:</label>
-        <input
-          onChange={handleTextChange}
-          name={'verifyPassword'}
-          placeholder={'verify password'}
-          type={'password'}
-          value={verifyPassword}
-        />
-        <button disabled={!!error}>Create Account</button>
-      </form>
-      {createAccountError && (<p>Error Creating Account</p>)}
-    </>
+    <div>
+      <SignupForm/>
+    </div>
   );
 };
 
