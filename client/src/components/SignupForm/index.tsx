6f014b7d04b4303098f938c01b22412ec821ff61
moon <rycmoon@gmail.com>
2022-04-12 19:06:43 -0500

    created signup and added a checker to the password

---
+++ client/src/components/SignupForm/index.tsx
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { CREATE_ACCOUNT } from '../../utils';
+import { useNavigate } from 'react-router-dom';
+
+interface ISignupForm {
+  username: string,
+  email: string,
+  password: string
+  verifyPassword: string
+}
+
+
+export const SignupForm = () => {
+
+
+  const navigation = useNavigate()
+  const [createAccount, { error: createAccountError }] = useMutation(CREATE_ACCOUNT);
+  const [error, setError] = useState<string>('');
+
+
+  const [signupForm, setSignupForm] = useState<ISignupForm>({
+    email: '',
+    password: '',
+    verifyPassword: '',
+    username: ''
+  });
+  const { email, password, verifyPassword, username } = signupForm;
+  useVerifyPassword(password,verifyPassword,setError)
+
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSignupForm((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+
+    if (!e.target.value.length) {
+      setError(`${e.target.name} is required`);
+    } else {
+      setError('');
+    }
+  };
+
+
+
+
+  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (error) {
+      return;
+    }
+    try {
+      //TODO
+      const { data } = await createAccount({ variables: { ...signupForm } });
+      navigation('/messenger')
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor={'username'}>Username:</label>
+        <input
+          onChange={handleTextChange}
+          name={'username'}
+          placeholder={'username'}
+          type={'username'}
+          value={username}
+        />
+        <label htmlFor={'email'}>Email:</label>
+        <input
+          onChange={handleTextChange}
+          name={'email'}
+          placeholder={'email'}
+          type={'email'}
+          value={email}
+        />
+        <label htmlFor={'password'}>Password:</label>
+        <input
+          onChange={handleTextChange}
+          name={'password'}
+          placeholder={'password'}
+          type={'password'}
+          value={password}
+        />
+        <label htmlFor={'verifyPassword'}>Verify Password:</label>
+        <input
+          onChange={handleTextChange}
+          name={'verifyPassword'}
+          placeholder={'verify password'}
+          type={'password'}
+          value={verifyPassword}
+        />
+        <button disabled={!!error}>Create Account</button>
+      </form>
+
+      {error && (<p>{error}</p>)}
+      {createAccountError && (<p>Error Creating Account</p>)}
+    </>
+  );
+};
+
+
+//Verify the users passwords match
+const useVerifyPassword = (password:string,verifyPassword:string,setError:(value:string)=>void) => {
+  useEffect(()=>{
+    console.log(password,verifyPassword)
+    if (password.localeCompare(verifyPassword) !== 0) {
+      setError('passwords do not match');
+    } else {
+      setError('');
+    }
+  },[password,verifyPassword])
+}
