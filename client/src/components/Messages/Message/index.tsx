d84ec2809cc1ddf0b46833a1e8dfe93c443b62d1
moon <rycmoon@gmail.com>
2022-04-15 20:00:46 -0500

    created a component that displays messages uniquly depending on if it is a user, the ai or if it is a loading message

---
+++ client/src/components/Messages/Message/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const styles = {
+  container: {
+    padding: '10px',
+    width:'fit-content',
+    margin:'5px',
+    color:'white'
+  },
+  userMessage:{
+    backgroundColor:'blue'
+  },
+  aiMessage:{
+    backgroundColor:'green'
+  }
+};
+
+
+interface props {
+  isUser?:boolean,
+  message?:string|undefined|null
+  loading?:boolean
+}
+
+const Message:React.FC<props> = ({isUser=false,message='',loading=false}) => {
+
+  if(loading){
+    return(
+      <div style={{ ...styles.container, backgroundColor:'gray'}}>
+        Thinking
+      </div>
+    )
+  }
+
+  const messageStyle = isUser ? styles.userMessage: styles.aiMessage
+
+  return (
+    <div style={{ ...styles.container,  ...messageStyle}}>
+      {message}
+    </div>
+  );
+};
+
+export default Message;
